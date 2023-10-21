536b72f6b57fef070716c113629d7e05457e4c54
小新 <782446723@qq.com>
2023-10-21 19:19:20 +0800

    --story=1010245 --user=小新ai footer部分 https://www.tapd.cn/49591392/s/1034768

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="footer flx-center">
-    <a href="https://github.com/NWAFU-Software-Engineering-Team" target="_blank"> 2023 © api-control-hub By NWAFU-Software-Engineering-Team. </a>
+    <a href="https://github.com/NWAFU-Software-Engineering-Team" target="_blank">
+      2023 © api-control-hub By NWAFU-Software-Engineering-Team.
+    </a>
   </div>
 </template>
 
