0f1a5a3330782480cdf7a19e311609a87b33c7b6
小新 <782446723@qq.com>
2023-10-26 21:12:41 +0800

    --story=1010432 --user=小新ai 个人空间 https://www.tapd.cn/49591392/s/1035159

--- src/views/myProject/mySpace/index.vue
+++ src/views/myProject/mySpace/index.vue
@@ -1,27 +1,38 @@
 <template>
-  <div class="card">
-    <div class="card"><h1>个人空间</h1></div>
-    <draggable
-      v-model="gridList"
-      class="card grid-container"
-      item-key="id"
-      animation="300"
-      chosen-class="chosen"
-      force-fallback="true"
-    >
-      <template #item="{ element }">
-        <div :class="'item' + ' ' + 'item-' + element.num">
-          {{ element.num }}
-        </div>
-      </template>
-    </draggable>
+  <div class="card my-space-container">
+    <h1>个人空间</h1>
+    <el-tabs v-model="activeName" class="my-space-tabs" @tab-click="handleClick">
+      <el-tab-pane label="团队项目" name="first">
+        <draggable
+          v-model="gridList"
+          class="card grid-container"
+          item-key="id"
+          animation="300"
+          chosen-class="chosen"
+          force-fallback="true"
+        >
+          <template #item="{ element }">
+            <div :class="'item' + ' ' + 'item-' + element.num">
+              {{ element.num }}
+            </div>
+          </template>
+        </draggable>
+      </el-tab-pane>
+      <el-tab-pane label="成员/权限" name="second">Config</el-tab-pane>
+      <el-tab-pane label="团队设置" name="third">Role</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script setup lang="ts" name="mySpace">
 import { ref } from "vue";
 import draggable from "vuedraggable";
+import type { TabsPaneContext } from "element-plus";
+const activeName = ref("first");
 
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event);
+};
 let gridList = ref([
   { id: 1, num: 1 },
   { id: 2, num: 2 },
