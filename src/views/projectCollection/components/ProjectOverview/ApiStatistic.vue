995686d6379e28cd4dce60a115062c1fa4b9626c
小新 <782446723@qq.com>
2023-11-03 16:53:13 +0800

    --story=1010435 --user=小新ai 项目概览界面 https://www.tapd.cn/49591392/s/1035308

--- src/views/projectCollection/components/ProjectOverview/ApiStatistic.vue
+++ src/views/projectCollection/components/ProjectOverview/ApiStatistic.vue
@@ -1,30 +1,32 @@
 <template>
-  <el-row>
-    <el-col :span="6">
-      <el-statistic title="接口数目" :value="props.apiCount" />
-    </el-col>
-    <el-col :span="6">
-      <el-statistic title="Mock接口数目" :value="props.mockCount" />
-    </el-col>
-    <el-col :span="6">
-      <el-statistic title="今日Mock调用次数" :value="props.dailyMockCount">
-        <template #suffix>
-          <el-icon style="vertical-align: -0.125em">
-            <Clock />
-          </el-icon>
-        </template>
-      </el-statistic>
-    </el-col>
-    <el-col :span="6">
-      <el-statistic title="总Mock调用次数" :value="props.totalMockCount">
-        <template #suffix>
-          <el-icon style="vertical-align: -0.125em">
-            <Connection />
-          </el-icon>
-        </template>
-      </el-statistic>
-    </el-col>
-  </el-row>
+  <div class="statistic-box">
+    <el-row>
+      <el-col :span="6">
+        <el-statistic title="接口数目" :value="props.apiCount" />
+      </el-col>
+      <el-col :span="6">
+        <el-statistic title="Mock接口数目" :value="props.mockCount" />
+      </el-col>
+      <el-col :span="6">
+        <el-statistic title="今日Mock调用次数" :value="props.dailyMockCount">
+          <template #suffix>
+            <el-icon style="vertical-align: -0.125em">
+              <Clock />
+            </el-icon>
+          </template>
+        </el-statistic>
+      </el-col>
+      <el-col :span="6">
+        <el-statistic title="总Mock调用次数" :value="props.totalMockCount">
+          <template #suffix>
+            <el-icon style="vertical-align: -0.125em">
+              <Connection />
+            </el-icon>
+          </template>
+        </el-statistic>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script setup lang="ts">
 import { Connection, Clock } from "@element-plus/icons-vue";
@@ -47,4 +49,8 @@ const props = defineProps({
   }
 });
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.statistic-box {
+  margin-bottom: 20px;
+}
+</style>
