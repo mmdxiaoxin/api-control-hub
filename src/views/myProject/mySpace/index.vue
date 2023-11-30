e8f63decb5f0b28c8b3e283c108152a5a7e1640f
小新 <782446723@qq.com>
2023-11-30 20:28:18 +0800

    完成个人空间的项目展示。

--- src/views/myProject/mySpace/index.vue
+++ src/views/myProject/mySpace/index.vue
@@ -15,8 +15,32 @@
           force-fallback="true"
         >
           <template #item="{ element }">
-            <div :class="'item' + ' ' + 'item-' + element.num">
-              {{ element.num }}
+            <div class="card-item">
+              <div class="card-header">
+                <div class="card-icon">
+                  <img :src="element.icon" alt="图标不存在" />
+                </div>
+                <div class="card-operation">
+                  <el-button :icon="Position" circle></el-button>
+                  <el-button type="warning" :icon="Star" circle></el-button>
+                  <el-dropdown ref="operation">
+                    <span class="el-dropdown-link">
+                      <el-button :icon="More" text></el-button>
+                    </span>
+                    <template #dropdown>
+                      <el-dropdown-menu>
+                        <el-dropdown-item>Action 1</el-dropdown-item>
+                        <el-dropdown-item>Action 2 </el-dropdown-item>
+                        <el-dropdown-item>Action 3</el-dropdown-item>
+                        <el-dropdown-item>Action 4</el-dropdown-item>
+                        <el-dropdown-item>Action 5</el-dropdown-item>
+                      </el-dropdown-menu>
+                    </template>
+                  </el-dropdown>
+                </div>
+              </div>
+              <div class="card-title">{{ element.title || "Error: Title not provided" }}</div>
+              <el-tag class="card-tag">{{ element.role || "HTTP" }}</el-tag>
             </div>
           </template>
         </draggable>
@@ -35,29 +59,34 @@
 <script setup lang="ts" name="mySpace">
 import { ref } from "vue";
 import draggable from "vuedraggable";
-import type { TabsPaneContext } from "element-plus";
+import type { DropdownInstance, TabsPaneContext } from "element-plus";
 import { getTagType } from "@/utils/workPlace";
 import { useWorkPlaceStore } from "@/stores/modules/workPlace";
 import TeamStatistics from "./components/TeamStatistics.vue";
 import TeamTable from "./components/TeamTable.vue";
 import TeamSetting from "./components/TeamSetting.vue";
+import { More, Position, Star } from "@element-plus/icons-vue";
 
 const workPlace = useWorkPlaceStore();
 
 const activeName = ref("first");
+const operation = ref<DropdownInstance>();
 const handleClick = (tab: TabsPaneContext, event: Event) => {
   console.log(tab, event);
 };
 let gridList = ref([
-  { id: 1, num: 1 },
-  { id: 2, num: 2 },
-  { id: 3, num: 3 },
-  { id: 4, num: 4 },
-  { id: 5, num: 5 },
-  { id: 6, num: 6 },
-  { id: 7, num: 7 },
-  { id: 8, num: 8 },
-  { id: 9, num: 9 }
+  { id: 1, title: "企业网站建设", icon: "src/assets/icons/xianxingdaoyu.svg" },
+  { id: 2, title: "电商平台开发", icon: "src/assets/icons/xianxingdiqiu.svg" },
+  { id: 3, title: "社交媒体应用", icon: "src/assets/icons/xianxingditu.svg" },
+  { id: 4, title: "在线教育平台", icon: "src/assets/icons/xianxingfanchuan.svg" },
+  { id: 5, title: "智能家居控制系统", icon: "src/assets/icons/xianxingfeiji.svg" },
+  { id: 6, title: "健康管理应用", icon: "src/assets/icons/xianxinglvhangriji.svg" },
+  { id: 7, title: "旅游信息平台", icon: "src/assets/icons/xianxingtianqiyubao.svg" },
+  { id: 8, title: "智能物流系统", icon: "src/assets/icons/xianxingxiangjipaizhao.svg" },
+  { id: 9, title: "人才招聘平台", icon: "src/assets/icons/xianxingxiarilengyin.svg" },
+  { id: 10, title: "金融数据分析工具", icon: "src/assets/icons/xianxingyoulun.svg" },
+  { id: 11, title: "在线购物商城", icon: "src/assets/icons/xianxingdaoyu.svg" },
+  { id: 12, title: "智能车辆管理系统", icon: "src/assets/icons/xianxingdaoyu.svg" }
 ]);
 </script>
 
