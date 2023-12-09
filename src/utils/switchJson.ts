069a0a53beda93529e1fbe32b20a28ed5f2094d6
小新 <782446723@qq.com>
2023-12-09 20:55:45 +0800

    fix: 删除无用工具类函数。

--- src/utils/switchJson.ts
+++ src/utils/switchJson.ts
@@ -1,4 +1,4 @@
-import { reactive, toRaw } from "vue";
+import { reactive } from "vue";
 
 // 将 JSON 字符串转换为 reactive 对象
 export const JSONToReactive = (jsonString: string): Record<string, any> | null => {
@@ -14,10 +14,4 @@ export const JSONToReactive = (jsonString: string): Record<string, any> | null =
     console.error("Error parsing JSON:", error);
     return null;
   }
-};
-
-// 将 reactive 转换为 JSON 字符串
-export const reactiveToJSON = (obj: Record<string, any>): string => {
-  const plainObject = toRaw(obj);
-  return JSON.stringify(plainObject);
 };
