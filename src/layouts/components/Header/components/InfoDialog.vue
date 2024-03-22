2d4980c1a4dde98236fdd5e86bafa3870823a409
小新 <782446723@qq.com>
2024-03-22 10:52:14 +0800

    refactor: 重命名模块

--- src/layouts/components/Header/components/InfoDialog.vue
+++ src/layouts/components/Header/components/InfoDialog.vue
@@ -39,7 +39,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useUserStore } from "@/stores/modules/user";
-import { useWorkPlaceStore } from "@/stores/modules/workPlace";
+import { useWorkPlaceStore } from "@/stores/modules/workbench";
 import { getRoleTagType } from "@/utils/workPlace";
 
 const useStore = useUserStore();
