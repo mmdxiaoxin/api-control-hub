5e3cfecd1099a8c0bd635d051d4515593d325c24
小新 <782446723@qq.com>
2024-10-02 19:54:28 +0800

    feat: 切换api时重置响应体

--- src/views/http-client/components/ApiConfig.vue
+++ src/views/http-client/components/ApiConfig.vue
@@ -152,13 +152,138 @@
       </el-row>
     </el-form>
     <!-- HTTP Response -->
-    <ApiResponse :http-response="httpResp" :http-error="httpErr" :http-status="httpStatus" />
+    <div class="response card">
+      <!-- 响应内容、Cookies 和响应头选项卡 -->
+      <el-tabs v-model="configResp">
+        <el-tab-pane label="Body" :name="ConfigType.Body">
+          <div class="body">
+            <div class="toolBar">
+              <el-radio-group v-model="resBodyRadio" size="small">
+                <el-radio-button value="Pretty">Pretty</el-radio-button>
+                <el-radio-button value="Raw">Raw</el-radio-button>
+                <el-radio-button value="Preview">Preview</el-radio-button>
+              </el-radio-group>
+              <el-select
+                v-model="ResSelect"
+                size="small"
+                v-if="resBodyRadio === 'Pretty'"
+                class="select"
+              >
+                <!-- 选择响应体格式 -->
+                <el-option
+                  v-for="item in ResFileOption"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                >
+                  <span>{{ item.label }}</span>
+                </el-option>
+              </el-select>
+            </div>
+            <div v-if="httpResp.status === 0 || !httpStatus">
+              <el-empty :image-size="70" />
+            </div>
+            <!-- JSON 视图，pretty -->
+            <JsonViewer
+              style="min-height: calc(100vh - 700px)"
+              :value="httpResp.data"
+              copyable
+              boxed
+              sort
+              :theme="globalStore.isDark ? 'dark' : 'light'"
+              v-if="
+                resBodyRadio === 'Pretty' &&
+                ResSelect === 'JSON' &&
+                httpResp.status !== 0 &&
+                httpStatus
+              "
+            />
+            <!-- JSON 视图，raw -->
+            <div
+              class="raw-json"
+              v-if="
+                resBodyRadio === 'Raw' &&
+                ResSelect === 'JSON' &&
+                httpResp.status !== 0 &&
+                httpStatus
+              "
+            >
+              {{ httpResp.data || "" }}
+            </div>
+            <!-- 非 JSON 格式的提示 -->
+            <div v-if="ResSelect !== 'JSON'">
+              <p>暂不支持！</p>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="Cookies" :name="ConfigType.Cookie">
+          <div class="response-params">No Cookies</div>
+        </el-tab-pane>
+        <el-tab-pane label="Headers" :name="ConfigType.Header">
+          <div class="response-params">
+            <!-- 响应头参数 -->
+            <el-table :data="responseHeader" border style="width: 100%">
+              <el-table-column prop="key" label="key" />
+              <el-table-column prop="value" label="Value" />
+            </el-table>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+      <div class="status">
+        <div class="item">
+          <span class="label">Status:</span>
+          <span class="value">
+            {{ httpStatus ? httpResp.status : httpErr.response?.status }}
+          </span>
+        </div>
+        <div class="item">
+          <span class="label">Time:</span>
+          <span class="value">
+            {{ httpStatus ? httpResp.duration : httpErr.duration }}
+            ms
+          </span>
+        </div>
+        <el-tooltip :effect="toolTipTheme" placement="top-start" :open-delay="500">
+          <template #content>
+            <div v-if="httpStatus" class="tooltip-content">
+              <div class="tooltip-section">
+                <h3 class="tooltip-title">Response Size</h3>
+                <p class="tooltip-item">
+                  Body: <span>{{ responseBodySize }}</span>
+                </p>
+                <p class="tooltip-item">
+                  Headers: <span>{{ responseHeadersSize }}</span>
+                </p>
+              </div>
+              <el-divider />
+              <div class="tooltip-section">
+                <h3 class="tooltip-title">Request Size</h3>
+                <p class="tooltip-item">
+                  Body: <span>{{ requestBodySize }}</span>
+                </p>
+                <p class="tooltip-item">
+                  Headers: <span>{{ requestHeadersSize }}</span>
+                </p>
+              </div>
+            </div>
+            <div v-else class="tooltip-content">
+              <h3 class="tooltip-title">Error</h3>
+              <el-divider />
+              <p class="tooltip-item">{{ httpErr.message }}</p>
+            </div>
+          </template>
+          <div class="item">
+            <span class="label">Size:</span>
+            <span class="value">{{ httpStatus ? responseSize : "Error" }}</span>
+          </div>
+        </el-tooltip>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import ApiResponse from "./ApiResponse.vue";
-import { onMounted, ref, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { getHttpConfigApi, updateHttpConfig } from "@/api/modules/http";
 import AxiosService, {
   RequestConfig,
@@ -166,13 +291,23 @@ import AxiosService, {
   ResponseWithError
 } from "@/utils/request";
 import { Connection, Edit, MessageBox } from "@element-plus/icons-vue";
-import { authOptions, bodyOptions, labelStyles, methods } from "@/views/http-client/config";
+import {
+  authOptions,
+  bodyOptions,
+  labelStyles,
+  methods,
+  ResFileOption
+} from "@/views/http-client/config";
 import MonacoEditor from "@/components/MonacoEditor/MonacoEditor";
 import QueryTable from "@/views/http-client/components/QueryTable.vue";
 import { ElMessage, FormInstance } from "element-plus";
 import { Http } from "@/api/interface";
 import { AxiosRequestConfig } from "axios";
 import { RequestForm } from "@/views/http-client/components/types";
+import { useGlobalStore } from "@/stores/modules/global";
+import { formatBytes } from "@/utils/apiConfig";
+import { JsonViewer } from "vue3-json-viewer";
+import "vue3-json-viewer/dist/index.css";
 
 const props = defineProps({
   itemId: {
@@ -190,7 +325,8 @@ enum ConfigType {
   Query = "query",
   Auth = "auth",
   Header = "header",
-  Body = "body"
+  Body = "body",
+  Cookie = "cookie"
 }
 
 enum BodyType {
@@ -226,16 +362,7 @@ const requestForm = ref<RequestForm>({
 const handleSave = async () => {
   const req: Http.ReqUpdate = {
     apiId: parseInt(props.itemId),
-    requestMethod: requestForm.value.requestMethod,
-    apiUrl: requestForm.value.apiUrl,
-    authType: requestForm.value.authType,
-    queryParams: requestForm.value.queryParams,
-    queryHeaders: requestForm.value.queryHeaders,
-    queryBodyForm: requestForm.value.queryBodyForm,
-    queryBodyFormX: requestForm.value.queryBodyFormX,
-    queryJsonBody: requestForm.value.queryJsonBody,
-    queryXmlBody: requestForm.value.queryXmlBody,
-    queryRawBody: requestForm.value.queryRawBody
+    ...requestForm.value
   };
   const res = await updateHttpConfig(req);
   if (res.code === 200) {
@@ -323,25 +450,77 @@ const sendRequest = async () => {
 // ============================= HTTP Response =============================
 
 const httpStatus = ref<boolean>(true);
-const httpResp = ref<ResponseWithDetails>();
-const httpErr = ref<ResponseWithError>();
+const httpResp = ref<ResponseWithDetails>({
+  status: 0,
+  duration: 0,
+  responseBodySize: 0,
+  responseHeadersSize: 0,
+  requestBodySize: 0,
+  requestHeadersSize: 0
+});
+const httpErr = ref<ResponseWithError>({
+  status: 0,
+  duration: 0,
+  requestBodySize: 0,
+  requestHeadersSize: 0
+});
+
+const configResp = ref(ConfigType.Body);
+const resBodyRadio = ref("Pretty");
+const ResSelect = ref("JSON");
+const globalStore = useGlobalStore();
+
+const requestBodySize = computed(() => formatBytes(httpResp.value.requestBodySize));
+const requestHeadersSize = computed(() => formatBytes(httpResp.value.requestHeadersSize));
+const responseSize = computed(() =>
+  formatBytes(httpResp.value.responseBodySize + httpResp.value.responseHeadersSize)
+);
+const responseBodySize = computed(() => formatBytes(httpResp.value.responseBodySize));
+const responseHeadersSize = computed(() => formatBytes(httpResp.value.responseHeadersSize));
+const responseHeader = computed(() => {
+  const headers = httpResp.value.headers || {};
+  return Object.keys(headers).map(key => {
+    return {
+      key,
+      value: headers[key]
+    };
+  });
+});
+const toolTipTheme = computed(() => {
+  return globalStore.isDark ? "dark" : "light";
+});
 
 // ============================= INIT =============================
 
-async function fetchApiConfig(apiId: string) {
+async function init(apiId: string) {
   const { data } = await getHttpConfigApi({ apiId });
   const { name, ...rest } = data;
   title.value = name;
   requestForm.value = rest;
+  httpStatus.value = true;
+  httpResp.value = {
+    status: 0,
+    duration: 0,
+    responseBodySize: 0,
+    responseHeadersSize: 0,
+    requestBodySize: 0,
+    requestHeadersSize: 0
+  };
+  httpErr.value = {
+    status: 0,
+    duration: 0,
+    requestBodySize: 0,
+    requestHeadersSize: 0
+  };
 }
 
 onMounted(() => {
-  fetchApiConfig(props.itemId);
+  init(props.itemId);
 });
 watch(
   () => props.itemId,
   () => {
-    fetchApiConfig(props.itemId);
+    init(props.itemId);
   },
   { immediate: true }
 );
@@ -352,5 +531,66 @@ watch(
   width: calc(100% - 260px);
   height: 100%;
   padding: 3%;
+  .response {
+    position: relative;
+    margin-top: 10px;
+    .status {
+      position: absolute;
+      top: 20px;
+      right: 10px;
+      display: flex;
+      .item {
+        display: flex;
+        align-items: center;
+        margin-right: 15px;
+        font-size: 14px;
+        color: var(--el-text-color-regular);
+        .label {
+          margin-right: 5px;
+          font-weight: bold;
+        }
+        .value {
+          color: var(--el-text-color-regular);
+        }
+      }
+    }
+    .body {
+      height: 100%;
+      .toolBar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 20px;
+        .select {
+          max-width: 20%;
+        }
+      }
+      .raw-json {
+        white-space: pre;
+      }
+    }
+  }
+  .tooltip-content {
+    font-family: "Noto Sans SC", "思源黑体", sans-serif;
+    background-color: var(--el-popper-bg-color);
+    .tooltip-section {
+      margin-bottom: 10px;
+      .tooltip-title {
+        margin-bottom: 5px;
+        font-size: 16px;
+        font-weight: bold;
+        color: var(--el-text-color-primary);
+      }
+      .tooltip-item {
+        margin: 0;
+        font-size: 14px;
+        color: var(--el-text-color-regular);
+        span {
+          font-weight: bold;
+          color: var(--el-text-color-regular);
+        }
+      }
+    }
+  }
 }
 </style>
